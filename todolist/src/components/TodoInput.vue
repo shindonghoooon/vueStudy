eccd5b5098ec2065ae4a040257c5b54d485196e0
donghoon <sdh940202@gmail.com>
2022-01-16 15:36:26 +0900

    push project todolist

---
+++ todolist/src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      //저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem=""; //비워주기(초기화)
+
+    }
+  }
+}
+</script>
---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+        {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
